03dfd74050d4860173c391e280530611b19e8a78
DungBuiTien1999 <nguyentonhi199@gmail.com>
2021-05-29 12:07:45 +0700

    fix bug EditAccount_Test_2

---
+++ Object Repository/Page_Town Square - dangerous world Mattermost/span_Edit.rs
@@ -0,0 +1,82 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>span_Edit</name>
+   <tag></tag>
+   <elementGuidId>fb1a3ebf-7c9d-49c7-bce5-c1df9ca14534</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//button[@id='usernameEdit']/span</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>#usernameEdit > span</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>span</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Edit</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;usernameEdit&quot;)/span[1]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//button[@id='usernameEdit']/span</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Username'])[1]/following::span[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)=concat('Click ', &quot;'&quot;, 'Edit', &quot;'&quot;, ' to add your full name')])[1]/following::span[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='dungtien'])[1]/preceding::span[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Nickname'])[1]/preceding::span[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[4]/div/div/button/span</value>
+   </webElementXpaths>
+</WebElementEntity>
--- Scripts/EditAcount/Test_5/Script1622263946089.groovy
+++ Scripts/EditAcount/Test_5/Script1622263946089.groovy
@@ -17,25 +17,3 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.callTestCase(findTestCase('SignUp/Test_11'), [:], FailureHandling.STOP_ON_FAILURE)
-
-WebUI.callTestCase(findTestCase('Login/Test_11'), [:], FailureHandling.STOP_ON_FAILURE)
-
-WebUI.click(findTestObject('Object Repository/Page_Town Square - dangerous world Mattermost/svg'))
-
-WebUI.click(findTestObject('Object Repository/Page_Town Square - dangerous world Mattermost/button_Account Settings'))
-
-WebUI.click(findTestObject('Object Repository/Page_Town Square - dangerous world Mattermost/span_Edit'))
-
-WebUI.setText(findTestObject('Object Repository/Page_Town Square - dangerous world Mattermost/input_Username_username'), 
-    'dungtienbui123')
-
-WebUI.click(findTestObject('Object Repository/Page_Town Square - dangerous world Mattermost/span_Save'))
-
-WebUI.click(findTestObject('Object Repository/Page_Town Square - dangerous world Mattermost/span_'))
-
-WebUI.verifyElementText(findTestObject('Object Repository/Page_Town Square - dangerous world Mattermost/div_dungtienbui123'), 
-    '@dungtienbui123')
-
-WebUI.closeBrowser()
-
